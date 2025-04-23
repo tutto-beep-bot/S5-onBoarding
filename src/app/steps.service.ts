e091cee95531b308f411e4564e0233e936b040de
Davi Lessa <davilessa.br@gmail.com>
2025-04-23 11:07:46 +0200

    Card centered, styled and fully working. Next and Previous buttons are functional.

--- src/app/steps.service.ts
+++ src/app/steps.service.ts
@@ -13,19 +13,19 @@ export class StepsService {
     return [
       {
         title: 'Dedica moltes hores',
-        description: 'Un mínim de 30 hores a la setmana. Si no en tens prou...',
+        description: 'Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicar-li més hores. Al principi sembla impossible, però notaràs una millora ràpidament.',
         image: 'assets/time_managment.svg',
         bgcolor: '#4da2a9'
       },
       {
         title: 'Programa projectes propis:',
-        description: 'Més val 10 hores treballant en projectes propis, que 10 hores mirant tutorials...',
+        description: 'Més val 10 hores treballant en projectes propis, que 10 hores mirant tutorials. La motivació i la implicació en el projecte ajudarà a accelerar el teu aprenentatge.',
         image: 'assets/programming.svg',
         bgcolor: '#d3d4d9'
       },
       {
         title: 'Procura descansar:',
-        description: 'Descansar bé i desconnectar són vitals. D\'aquesta manera reduiràs l\'estrès...',
+        description: "Descansar bé i desconnectar són vitals. D'aquesta manera reduiràs l'estrès i l'ansietat. Milloraràs la teva concentració i consolidaràs el teu aprenentatge.",
         image: 'assets/meditation.svg',
         bgcolor: '#ffd167'
       }
