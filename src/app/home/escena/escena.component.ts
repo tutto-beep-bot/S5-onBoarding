3bed8917b6ceeafcb852c07c05222b2e3b560739
Davi Lessa <davilessa.br@gmail.com>
2025-04-24 12:08:11 +0200

    card animation now implemented and working.

--- src/app/home/escena/escena.component.ts
+++ src/app/home/escena/escena.component.ts
@@ -1,13 +1,23 @@
 import { Component, Input } from '@angular/core';
 import { NgFor, NgIf, NgStyle } from '@angular/common'
 import { iStep } from '../../iStep';
+import { trigger, state, transition, style, animate } from '@angular/animations'
 
 @Component({
   selector: 'app-escena',
   imports: [NgFor, NgIf, NgStyle],
   templateUrl: './escena.component.html',
-  styleUrl: './escena.component.css'
+  styleUrl: './escena.component.css',
+  animations: [
+    trigger('cardTransition', [
+      transition('* => *', [
+        style({ opacity: 0, transform: 'translateX(20px)' }),
+        animate('300ms ease-out', style({ opacity: 1, transform: 'translateX(0)' }))
+      ])
+    ])
+  ]
 })
+
 export class EscenaComponent {
   @Input() tips: iStep[] = []
   currentStep: number = 0;
